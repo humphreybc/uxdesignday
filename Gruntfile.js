30e88641d5752011679bb82cfa88598e68caa61a
Benjamin Humphrey <humphreybc@gmail.com>
2013-03-30 20:56:07 +1300

    Add Grunt and package.json for watching changes

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      scripts: {
+        files: {
+          './public/js/app.js': './js/app.coffee'
+        }
+      }
+    },
+    coffeelint: {
+      app: ['./*.coffee'],
+      options: {
+        indentation: {
+          value: 2,
+          level: 'error'
+        },
+        max_line_length: {
+          value: 120,
+          level: 'error'
+        }
+      }
+    },
+    stylus: {
+      compile: {
+        files: {'./public/css/app.css': './css/app.styl'}
+      }
+    },
+    watch: {
+      scripts: {
+        files: './js/*.coffee',
+        tasks: ['coffeelint', 'coffee:scripts']
+      },
+      styles: {
+        files: './css/**/*.styl',
+        tasks: ['stylus']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  grunt.registerTask('default', []);
+
+  grunt.registerTask('dev', [
+    'watch'
+  ]);
+};
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-uxdesignday
-===========
+UX Design Day
+=============
+[beta.uxdesignday.org](http://beta.uxdesignday.org)
 
-Website for uxdesignday.org
+Source code for uxdesignday.org - static HTML, CSS, and a little bit of JS.
+
+### Dependencies
+
+Uses [Coffeescript](http://coffeescript.org/) and [Stylus](http://learnboost.github.com/stylus/). I compile with [LiveReload](http://livereload.com/) which is pretty great, but any Coffeescript / Stylus compiler will do.
+
+### Using Grunt
+
+A Gruntfile with a `dev` task is available for development. Install the node prerequisites with `npm install` and run `grunt dev` to watch changes to CoffeeScript and Stylus files.
