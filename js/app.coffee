f12e61a0477241e4bc02579ca5953d24c34b8ad5
Benjamin Humphrey <humphreybc@gmail.com>
2013-03-30 19:06:56 +1300

    Initial stab at the layout, add font icons in, add in a few js helpers

---
+++ js/app.coffee
@@ -0,0 +1,1 @@
+console.log 'Like looking under the hood? Feel free to help make this site better at https://github.com/humphreybc/uxdesignday'
---
+++ public/js/ie-icons.js
@@ -0,0 +1,32 @@
+/* Load this script using conditional IE comments if you need to support IE 7 and IE 6. */
+
+window.onload = function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'uxdesignday\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+			'icon-ticket' : '&#xe000;',
+			'icon-users' : '&#xe001;',
+			'icon-flag' : '&#xe002;',
+			'icon-calendar' : '&#xe003;',
+			'icon-facebook' : '&#xe004;',
+			'icon-twitter' : '&#xe005;',
+			'icon-googleplus' : '&#xe006;',
+			'icon-github' : '&#xe007;'
+		},
+		els = document.getElementsByTagName('*'),
+		i, attr, html, c, el;
+	for (i = 0; i < els.length; i += 1) {
+		el = els[i];
+		attr = el.getAttribute('data-icon');
+		if (attr) {
+			addIcon(el, attr);
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+};
