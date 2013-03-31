a0265aa4b993409661bb55c8233b0603373c4d99
Sam Tran <samvtran@gmail.com>
2013-03-30 20:31:29 -0500

    Fixing up navigation highlight and disabling on clients < 600px wide

--- js/app.coffee
+++ js/app.coffee
@@ -1,35 +1,46 @@
+animating = false
+linkOffsets = []
+
 $(document).ready ->
   console.log 'Like looking under the hood? Feel free to help make this site better at https://github.com/humphreybc/uxdesignday'
 
   $('#header a').click ->
+    animating = true
     $('html, body').animate
       scrollTop: ($($(this).attr('href')).offset().top) - 100
-    , 500
+    , 500, ->
+      checkOffset $(window).scrollTop()
+      animating = false;
     false
 
-  linkOffsets = []
-  navLinks = $('#nav-links li a')
-  for item in navLinks
-    link = $(item).attr('href')
-    # ~~ forces integer context so we don't accidentally subtract in string context
-    linkOffsets.push([link, if typeof $(link).offset() is 'undefined' then null else ~~$(link).offset().top - 100])
+$(window).load ->
+  if document.body.clientWidth > 600
+    navLinks = $('#nav-links li a')
+    for item in navLinks
+      link = $(item).attr('href')
+      # ~~ forces integer context so we don't accidentally subtract in string context
+      linkOffsets.push([link, if typeof $(link).offset() is 'undefined' then null else ~~$(link).offset().top - 100])
+    linkOffsets.reverse() # Going bottom up so we catch the active link furthest down the page
 
-  checkOffset = (scrollTop) ->
+    checkOffset $(window).scrollTop()
+
+    timeout = null
+    $(window).scroll ->
+      scrollTop = $(this).scrollTop();
+      if !timeout && !animating
+        timeout = setTimeout ->
+          timeout = null
+          checkOffset scrollTop
+        , 250
+
+checkOffset = (scrollTop) ->
+    breakLoop = false;
     for link in linkOffsets
       return if link[1] is null
-      if scrollTop > link[1]
+      if scrollTop + 10 > link[1]
         activeLink = $('a[href="' + link[0] + '"]')
         unless activeLink.hasClass 'active'
           $('a.active').removeClass 'active'
           activeLink.addClass 'active'
-
-  checkOffset $(window).scrollTop()
-
-  timeout = null
-  $(window).scroll ->
-    scrollTop = $(this).scrollTop();
-    if !timeout
-      timeout = setTimeout ->
-        timeout = null
-        checkOffset scrollTop
-      , 250
+        breakLoop = true
+      break if breakLoop
--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,69 @@
 (function() {
+  var animating, checkOffset, linkOffsets;
+
+  animating = false;
+
+  linkOffsets = [];
 
   $(document).ready(function() {
-    var checkOffset, item, link, linkOffsets, navLinks, timeout, _i, _len;
     console.log('Like looking under the hood? Feel free to help make this site better at https://github.com/humphreybc/uxdesignday');
-    $('#header a').click(function() {
+    return $('#header a').click(function() {
+      animating = true;
       $('html, body').animate({
         scrollTop: ($($(this).attr('href')).offset().top) - 100
-      }, 500);
+      }, 500, function() {
+        checkOffset($(window).scrollTop());
+        return animating = false;
+      });
       return false;
     });
-    linkOffsets = [];
-    navLinks = $('#nav-links li a');
-    for (_i = 0, _len = navLinks.length; _i < _len; _i++) {
-      item = navLinks[_i];
-      link = $(item).attr('href');
-      linkOffsets.push([link, typeof $(link).offset() === 'undefined' ? null : ~~$(link).offset().top - 100]);
-    }
-    checkOffset = function(scrollTop) {
-      var activeLink, _j, _len1;
-      for (_j = 0, _len1 = linkOffsets.length; _j < _len1; _j++) {
-        link = linkOffsets[_j];
-        if (link[1] === null) {
-          return;
-        }
-        if (scrollTop > link[1]) {
-          activeLink = $('a[href="' + link[0] + '"]');
-          if (!activeLink.hasClass('active')) {
-            $('a.active').removeClass('active');
-            activeLink.addClass('active');
-          }
-        }
-      }
-    };
-    checkOffset($(window).scrollTop());
-    timeout = null;
-    return $(window).scroll(function() {
-      var scrollTop;
-      scrollTop = $(this).scrollTop();
-      if (!timeout) {
-        return timeout = setTimeout(function() {
-          timeout = null;
-          return checkOffset(scrollTop);
-        }, 250);
-      }
-    });
   });
+
+  $(window).load(function() {
+    var item, link, navLinks, timeout, _i, _len;
+    if (document.body.clientWidth > 600) {
+      navLinks = $('#nav-links li a');
+      for (_i = 0, _len = navLinks.length; _i < _len; _i++) {
+        item = navLinks[_i];
+        link = $(item).attr('href');
+        linkOffsets.push([link, typeof $(link).offset() === 'undefined' ? null : ~~$(link).offset().top - 100]);
+      }
+      linkOffsets.reverse();
+      checkOffset($(window).scrollTop());
+      timeout = null;
+      return $(window).scroll(function() {
+        var scrollTop;
+        scrollTop = $(this).scrollTop();
+        if (!timeout && !animating) {
+          return timeout = setTimeout(function() {
+            timeout = null;
+            return checkOffset(scrollTop);
+          }, 250);
+        }
+      });
+    }
+  });
+
+  checkOffset = function(scrollTop) {
+    var activeLink, breakLoop, link, _i, _len;
+    breakLoop = false;
+    for (_i = 0, _len = linkOffsets.length; _i < _len; _i++) {
+      link = linkOffsets[_i];
+      if (link[1] === null) {
+        return;
+      }
+      if (scrollTop + 10 > link[1]) {
+        activeLink = $('a[href="' + link[0] + '"]');
+        if (!activeLink.hasClass('active')) {
+          $('a.active').removeClass('active');
+          activeLink.addClass('active');
+        }
+        breakLoop = true;
+      }
+      if (breakLoop) {
+        break;
+      }
+    }
+  };
 
 }).call(this);
