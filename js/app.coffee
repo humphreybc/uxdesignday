77a2340507ba5a8971fa089206b338f025dc61c3
Benjamin Humphrey <humphreybc@gmail.com>
2013-03-31 13:36:39 +1300

    Clicking logo now takes you to the top

--- js/app.coffee
+++ js/app.coffee
@@ -1,7 +1,7 @@
 $(document).ready ->
   console.log 'Like looking under the hood? Feel free to help make this site better at https://github.com/humphreybc/uxdesignday'
 
-  $('#nav-links li a').click ->
+  $('#header a').click ->
     $('html, body').animate
       scrollTop: ($($(this).attr('href')).offset().top) - 100
     , 500
--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 
   $(document).ready(function() {
     console.log('Like looking under the hood? Feel free to help make this site better at https://github.com/humphreybc/uxdesignday');
-    return $('#nav-links li a').click(function() {
+    return $('#header a').click(function() {
       $('html, body').animate({
         scrollTop: ($($(this).attr('href')).offset().top) - 100
       }, 500);
